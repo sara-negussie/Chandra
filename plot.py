33d47c4e21fb29affebc7d2df5296b843843e75a
Sara Negussie <snegussie@wesleyan.edu>
2022-08-12 20:04:22 -0400

    check ra and dec

--- plot.py
+++ plot.py
@@ -1,4 +1,3 @@
-from hashlib import new
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -29,12 +28,14 @@ sdssPetroMag_g = sdss['petroMag_g']
 #reorder array
 newArray = [] 
 for i in range(np.size(myRA)):
-    d_RA = decimal.Decimal(sdssRA[i])
-    decimals = d_RA.as_tuple().exponent
+    #d_RA = decimal.Decimal(sdssRA[i])
+    #decimals = d_RA.as_tuple().exponent
+    decimals = len(str(sdssRA[i]).split(".")[1])
     tmp1 = np.where( trunc(myRA,decimals=3) ==  trunc(sdssRA[i],decimals=3) ) 
     #print(tmp1)
-    d_DEC = decimal.Decimal(sdssDEC[i])
-    decimals = d_DEC.as_tuple().exponent
+    #d_DEC = decimal.Decimal(sdssDEC[i])
+    #decimals = d_DEC.as_tuple().exponent
+    decimals = len(str(sdssDEC[i]).split(".")[1])
     tmp2 = np.where( trunc(myDEC,decimals=3) ==  trunc(sdssDEC[i],decimals=3) ) 
     #print(tmp2)
     resultArray = (np.intersect1d(tmp1, tmp2))
@@ -53,6 +54,15 @@ for i in range(np.size(myRA)):
 #print(a)
 #Plotting Best Mag vs Petro Mag g
 #print(np.size(newArray))
+
+print("Writing file")
+
+with open(r'checkra&dec.txt', 'w') as fp:
+    fp.write("Best_Mag\tPetro_Mag_g\tDifference\tMy_RA\tCSC_RA\tMy_DEC\tCSC_DEC\n")
+    for j in range(np.size(newArray)):
+        fp.write("%d\t%d\t%d\t%d\t%d\t%d\t%d\n" %( (bestMag[i]), (sdssPetroMag_g[i]), (np.fabs(bestMag[i]-sdssPetroMag_g[i])), (sdssRA[i]), (myRA[j]), (sdssDEC[i]), (decGalaxies[j]) ) )
+
+
 plt.scatter(newArray, bestMag[0:np.size(newArray)], s = 5)
 plt.xlabel("PetroMag_g")
 plt.ylabel("Best Mag")
